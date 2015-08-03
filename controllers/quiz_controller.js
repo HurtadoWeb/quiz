6656baba202397904306d28595de7b255de53599
hurtazul <personal@juanantoniohurtado.es>
2015-08-03 17:49:39 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,13 +17,13 @@ exports.load = function(req, res, next, quizId){
 // GET /quizes
 exports.index = function(req, res) {
    models.Quiz.findAll().then(function(quizes) {  
-   res.render('quizes/', {quizes: quizes});
+   res.render('quizes/', {quizes: quizes, errors: []});
    }).catch(function(error) { next(error);})
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
- res.render('quizes/show', { quiz: req.quiz});
+ res.render('quizes/show', { quiz: req.quiz, errors: []});
  };
 
 // GET /quizes/:id/answer
@@ -32,7 +32,7 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
- res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+ res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 //GET /quizes/new
@@ -40,20 +40,50 @@ exports.new = function(req, res) {
   var quiz= models.Quiz.build (
       {pregunta:"Pregunta", respuesta:"Respuesta"}
     );
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 //GET /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build(req.body.quiz);
 
-  //aguarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then (function() {
-    res.redirect('/quizes');
-  }) //Redireccion HTTP (URL relativo) lista de preguntas
+  quiz.validate().then(function(err) {
+    if (err) {
+      res.render('quizes/new', {quiz: quiz, errors: err.errors});
+    }else{
+      //aguarda en DB los campos pregunta y respuesta de quiz
+      quiz.save({fields: ["pregunta", "respuesta"]}).then (function() {
+      res.redirect('/quizes')});
+    } 
+});
+};
+
+//GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz; //autoload de instancia de quiz
+
+  res.render("quizes/edit", {quiz: quiz, errors: []});
+}
+
+//PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz.validate().then(
+      function (err){
+        if (err){
+          res.render('/quizes/edit', {quiz: req.quiz, errors: err.errors});
+        }else{
+          req.quiz.save({fields:["pregunta", "respuesta"]}).then(
+              function() {res.redirect("/quizes");}
+            );
+        } //Redireccion HTTP a lista de preguntas (URL relativo)
+      }
+    ); 
 };
 
 //GET/ Author
 exports.author = function(req, res) {
-	res.render('author');
+	res.render('author', {errors: []});
 }
